ecda6a351e640cae42204874ad6418ffd259e4dc
Azazel <384735481@qq.com>
2026-03-31 19:36:55 +0800

    [fix] SU(Simple Update)
    1.移除了蓝牙库(用不上的)
    2.添加了.gitignore pom.xml

---
+++ src/main/java/com/azathorpe/app.java
@@ -0,0 +1,21 @@
+package com.azathorpe;
+
+import com.azathorpe.Utils.SerialPortUtils;
+
+import java.util.ArrayList;
+
+// 整个程序的入口 需要作为服务一直挂着这个程序
+// 整个程序原理就是监听串口 然后通过串口和单片机交互
+public class app {
+    public static ArrayList<Thread> threadPool = new ArrayList<>();
+
+    public static void main(String[] args) {
+        SerialPortUtils.serialPortInitialization();
+        System.out.println(SerialPortUtils.startListenMessageThread());
+
+        while(!threadPool.isEmpty()){
+
+        }
+
+    }
+}
